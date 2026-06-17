a30d2f9621203691da32954c63737c42250f69ae
HORIE <26223147+suikan4github@users.noreply.github.com>
2026-06-17 05:29:57 +0000

    Add toolbox to Fedora operation.
    Support both workstation and atomic.

--- lib/patch-ubuntu.sh
+++ lib/patch-ubuntu.sh
@@ -10,12 +10,9 @@ function patch-ubuntu() {
     cd $SRC
     sudo cat /sys/firmware/acpi/tables/SSDT4 > SSDT4.aml
 
-    sudo apt update && sudo apt install acpica-tools patch
-
-    # Disassemble the ACPI table to a human-readable format.
-    iasl -d SSDT4.aml
 
     # Create a patch file to modify the ACPI table.
+    echo "Creating patch file fujitsu-vdd.patch..."
     cat <<- EOF | tee fujitsu-vdd.patch
     --- SSDT4.dsl.orig    2024-07-29 18:33:14.782373152 +0200
     +++ SSDT4.dsl 2024-07-29 18:38:33.021477685 +0200
@@ -53,7 +50,16 @@ function patch-ubuntu() {
                 }
 EOF
 
+    # Install required packages for patching.
+    echo "Installing required packages for patching..."
+    sudo apt update && sudo apt install acpica-tools patch
+
+    # Disassemble the ACPI table to a human-readable format.
+    echo "Disassembling the ACPI table..."
+    iasl -d SSDT4.aml
+
     # Apply the patch to the disassembled ACPI table.
+    echo "Applying the patch to the disassembled ACPI table..."
     patch < fujitsu-vdd.patch
 
     # If fail, exit the shell function.
@@ -63,6 +69,7 @@ EOF
     fi
 
     # Reassemble the patched ACPI table back to binary format.
+    echo "Reassembling the patched ACPI table..."
     iasl -sa SSDT4.dsl 
 
     # Create a working directory.
@@ -70,24 +77,30 @@ EOF
 
     # Copy patched aml file to the working directory. 
     # The directory structure must be `kernel/firmware/acpi/`
+    echo "Copying the patched ACPI table to the working directory..."
     cp ${SRC}/SSDT4.aml ${CPIO_SRC}
 
     # Generate CPIO archive 
+    echo "Generating CPIO archive..."
     cd ${SRC}
     find kernel | cpio -H newc -o > ./acpi_override.cpio
 
     # Deploy the CPIO file.
+    echo "Deploying the CPIO file to /boot/acpi_override.cpio..."
     sudo cp ./acpi_override.cpio /boot/acpi_override.cpio
 
     # Add ACPI patch CPIO file to the initrd.  
     ADDITIONAL_INITRD='GRUB_EARLY_INITRD_LINUX_CUSTOM="acpi_override.cpio"'
     # Is additional line already present in /etc/default/grub? If not, append it.
     if ! grep -qF "$ADDITIONAL_INITRD" /etc/default/grub; then
+        echo "Adding ACPI patch CPIO file to the initrd in /etc/default/grub..."
         echo "$ADDITIONAL_INITRD" | sudo tee -a /etc/default/grub
     fi
 
     # Update GRUB configuration to include the new initrd.
+    echo "Updating GRUB configuration..."
     sudo update-grub
 
+    echo "ACPI patch applied successfully. Please reboot your system for the changes to take effect."
     return 0
 }
